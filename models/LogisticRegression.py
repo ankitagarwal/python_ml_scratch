2e8a4dbc347a801c6d56d632f541696ebe9cfdea
Ankit Agarwal <ankit.agrr@gmail.com>
2020-04-29 16:15:07 +0530

    Cost function

--- models/LogisticRegression.py
+++ models/LogisticRegression.py
@@ -1,10 +1,13 @@
 import numpy as np
 
+
 class LogisticRegression:
 
     """
     Module to implement Logistic Regression using stochastic Gradient Descent. Not using numpy products was an intent.
-    See: images/sigmoid
+    See:
+        images/sigmoid.png
+        images/logistic_regression_sigmoid_w_threshold.png
     """
 
     coefs = []
@@ -23,5 +26,36 @@ class LogisticRegression:
         Predict a single value. Basically this is sigmoid over linear.
         """
         linear = self.predict_single(x)
-        y_hat = 1/1 + e^(-linear)
+        y_hat = 1/1 + np.exp(-linear)
         return y_hat
+
+    def cost_function(self, y_hat: int, y: int) -> int:
+        """
+        We use cross entropy log-loss because of the reasons below instead of MSE.
+        Note:
+            If our cost function has many local minimums (MSE in this case),
+            gradient descent may not find the optimal global minimum.
+        Note:
+            The key thing to note is the cost function penalizes confident and wrong predictions more than
+            it rewards confident and right predictions! The corollary is increasing prediction accuracy
+            (closer to 0 or 1) has diminishing returns on reducing cost due to the logistic nature of
+            our cost function.
+        see:
+            images/lr_cost_fucn_0.png
+            images/lr_cost_fucn_1.png
+            images/lr_cost_fucn_2.png
+        """
+        first_term = y * np.log(y_hat)
+        second_term = (1 - y) * np.log(1 - y_hat)
+        cost = (first_term + second_term)
+        return cost
+
+    def average_cost(self, y_hats: [int], ys: [int]) -> int:
+        if len(y_hats) != len(ys):
+            raise RuntimeError ("Mismatch between length of predictions and data. "
+                                "Expected {}, found {}".format(len(y_hats), len(ys)))
+        cost = 0
+        for i in range(len(y_hats)):
+            cost += self.cost_function(y_hats[i], ys[i])
+        cost /= len(y_hats)
+        return cost
